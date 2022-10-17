b4f03e7589cccd4e8d6287b598754b95397bf73d
fadiabji <fadi.abji@htomail.com>
2022-10-17 10:27:29 +0200

    Add project files.

---
+++ MvcAssignment4/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAssignment4.Models;
+using System.Diagnostics;
+
+namespace MvcAssignment4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private static List<Entry> blogEntries = new List<Entry>()
+        {
+            new Entry()
+            {
+                Id = 1,
+                Category = "Coding",
+                Title = "Asp.net core",
+                Content = "Asp.net Core is the most popular web framwork, it is open-source"
+            },
+
+            new Entry()
+            {
+                Id = 2,
+                Category = "Design",
+                Title = "Strapping boots with Bootstrap",
+                Content = "Quickly design and customize responsive mobile-first approach sites"
+            }
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("listOfEntriesKey", string.Join(",", blogEntries));
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // Adding method and a view 
+        public IActionResult postsWall()
+        {
+            return View();
+        }
+
+        public IActionResult EntriesPartialViews()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ MvcAssignment4/Models/Entry.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace MvcAssignment4.Models
+{
+    public class Entry
+    {
+        public int  Id { get; set; }
+        public string? Category { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+    }
+}
